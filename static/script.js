cbc692161a699a871051e4b9fca2341608473ed6
camilafagundes <fagundescamilaferraz@gmail.com>
2025-03-26 20:28:18 -0300

    Menu responsivo

---
+++ static/script.js
@@ -0,0 +1,11 @@
+/* Abre fecha menu lateral dem modo mobile */
+
+const menuMobile = document.querySelector('.menu-mobile')
+const body = document.querySelector('body')
+
+menuMobile.addEventListener('click', () => {
+    menuMobile.classList.contains("bi-list")
+    ? menuMobile.classList.replace("bi-list", "bi-x")
+    : menuMobile.classList.replace("bi-x", "bi-list");
+    body.classList.toggle("mobile-nav-active");
+});
